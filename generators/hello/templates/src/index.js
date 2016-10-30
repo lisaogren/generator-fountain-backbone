ecc0713ab13ac64c1e59d5d0126c5b51bf2f219d
RasCarlito <rascarlito@gmail.com>
2016-10-30 18:03:38 +0100

    Add require/import calls for styles in hello generator

--- generators/hello/templates/src/index.js
+++ generators/hello/templates/src/index.js
@@ -3,6 +3,8 @@ var Backbone = require('backbone');
 
 var Hello = require('./app/hello');
 
+require('./index.<%- css %>');
+
 var world = new Backbone.Model({name: 'world'});
 
 var helloView = new Hello({model: world});
